937695c0c5b501c670b111ea732ff6ec5fddeac5
Dmitrii Markov <markovdima43@gmail.com>
2024-08-10 23:33:45 +0200

    Fix a bug

--- app/src/main/java/com/markoved/countries/domain/SearchCountriesByNameUseCase.kt
+++ app/src/main/java/com/markoved/countries/domain/SearchCountriesByNameUseCase.kt
@@ -4,6 +4,9 @@ class SearchCountriesByNameUseCase(
     private val countryRepository: CountryRepository
 ) {
     suspend operator fun invoke(name: String): List<Country> {
+        if (name.isBlank()) {
+            return countryRepository.getAllCountries()
+        }
         return countryRepository.getCountriesByName(name)
     }
 }
